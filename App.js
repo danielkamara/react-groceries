c1c585976276411e5474a5c901faaacecfbf6c2d
daniel <danlikestech@gmail.com>
2022-02-20 12:30:17 -0600

    added data file

--- App.js
+++ App.js
@@ -1,16 +1,79 @@
+console.table(items);
+
 class App extends React.Component {
   state = {
+    items: items,
     item: "",
     brand: "",
-    units: "",
+    units: 0,
     quantity: 0,
     isPurchased: false,
+    shoppingCart: [],
+  };
+
+  handleChange = (event) => {
+    console.dir(event);
+    this.setState({ [event.target.id]: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const newItem = {
+      brand: this.state.brand,
+      units: this.state.units,
+      quantity: this.state.quantity,
+    };
+    this.setState({
+      items: [newItem, ...this.state.items],
+      item: "",
+      brand: "",
+      units: 0,
+      quantity: 0,
+      isPurchased: false,
+    });
   };
 
   render() {
     return (
       <div className="container">
-        <h1>Hello</h1>
+        <h1>React Groceries</h1>
+
+        {/* Input Form */}
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="item">Item</label>
+          <input
+            type="text"
+            value={this.state.item}
+            id="item"
+            onChange={this.state.handleChange}
+          />
+          <br />
+
+          <label htmlFor="brand">Brand</label>
+          <input
+            type="text"
+            value={this.state.brand}
+            id="brand"
+            onChange={this.state.handleChange}
+          />
+          <br />
+
+          <label htmlFor="units">Units</label>
+          <input
+            type="number"
+            value={this.state.units}
+            onChange={this.state.handleChange}
+          />
+          <br />
+
+          <label htmlFor="quantity">Quantity</label>
+          <input
+            type="number"
+            value={this.state.quantity}
+            onChange={this.state.handleChange}
+          />
+          <input type="submit"></input>
+        </form>
       </div>
     );
   }
---
+++ data.js
@@ -0,0 +1,23 @@
+const items = [
+  {
+    item: "Cheese",
+    brand: "Kraft",
+    units: 2.39,
+    quantity: 3,
+    isPurchased: true,
+  },
+  {
+    item: "Milk",
+    brand: "Silk",
+    units: 4.99,
+    quantity: 2,
+    isPurchased: true,
+  },
+  {
+    item: "Bread",
+    brand: "Wonder",
+    units: 2.39,
+    quantity: 5,
+    isPurchased: false,
+  },
+];
